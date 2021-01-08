5e6f957c0ce80c02e1b718b1bfcacd3a98c75f3b
Yuichiro MUKAI <y.iky917@gmail.com>
2021-01-08 13:46:58 +0900

    Created a project. start coding!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenTKTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
